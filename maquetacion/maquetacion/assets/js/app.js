f103575fb5cbaf8832e23300b0f2fa84ff5a0f20
nantaarc24 <fernandotapiaarcos@outlook.es>
2022-04-28 18:06:47 -0500

    Proyecto finalizado

---
+++ maquetacion/maquetacion/assets/js/app.js
@@ -0,0 +1,29 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    //alert('Documento cargado');
+    menuMobile();
+})
+
+function menuMobile() {
+    const menu = document.querySelector('.menu-mobile');
+    menu.addEventListener('click', function () {
+        //alert('click en el menu mobile');
+        const navegacion = document.querySelector('.navegacion');
+        console.log(navegacion);
+        /*if (navegacion.classList.contains('mostrar')) {
+            navegacion.classList.remove('mostrar');
+            //console.log('tiene la clase mostrar');
+        } else {
+            //console.log('no tiene la clase mostrar');
+            navegacion.classList.add('mostrar');
+        }*/
+        navegacion.classList.toggle('mostrar');
+    });
+}
+
+function configBotonEnviar() {
+    const boton = document.querySelector('.boton');
+    boton.addEventListener('click', function (e) {
+        alert('mensaje del boton');
+    });
+
+}
---
+++ maquetacion/maquetacion/assets/js/contacto.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    iniciarApp();
+})
+
+function iniciarApp() {
+    configBotonEnviar();
+}
+
+function configBotonEnviar() {
+    const boton = document.querySelector('.boton');
+    boton.addEventListener('click', function (e) {
+        alert('mensaje del boton');
+    });
+    
+}
